26a0f1e589cafc2d98201b6cef7ac50fbbedb182
jjii-nu <tonywoo616@gamil.com>
2024-05-13 18:58:18 +0900

    5week-hw

--- project/main/views.py
+++ project/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
-from .models import Post
+from .models import Post, Comment, Tag
 
 # Create your views here.
 
@@ -22,7 +22,20 @@ def new_post(request):
 
 def detail(request, id):
     post = get_object_or_404(Post, pk = id)
-    return render(request, 'main/detail.html', {'post':post})
+    if request.method == 'GET':
+        comments = Comment.objects.filter(post=post)
+        return render(request, 'main/detail.html', {'post':post, 'comments': comments})
+    
+    elif request.method == 'POST':
+        new_comment = Comment()
+
+        new_comment.post = post
+        new_comment.writer = request.user
+        new_comment.content = request.POST['content']
+        new_comment.pub_date = timezone.now()
+
+        new_comment.save()
+        return redirect('main:detail', id)
 
 def edit(request, id):
     edit_post = Post.objects.get(pk=id)
@@ -30,19 +43,17 @@ def edit(request, id):
 
 def update(request, id):
     update_post = Post.objects.get(pk=id)
+    if request.user.is_authenticated and request.user == update_post.writer:
+        update_post.title = request.POST.get('title')
+        update_post.email = request.POST.get('email')
+        update_post.body = request.POST.get('body')
+        update_post.pub_date = timezone.now()
 
-    update_post.title = request.POST.get('title')
-    update_post.writer = request.POST.get('writer')
-    update_post.email = request.POST.get('email')
-    update_post.body = request.POST.get('body')
-    update_post.pub_date = timezone.now()
-    update_post.image = request.FILES.get('image')
-
-    if request.FILES.get('image'):
-        update_post.image = request.FILES['image']
-
-    update_post.save()
-
+        if request.FILES.get('image'):
+            update_post.image = request.FILES['image']
+        
+        update_post.save()
+        return redirect('main:detail', update_post.id)
     return redirect('main:detail', update_post.id) 
 
 def delete(request, id):
@@ -54,7 +65,7 @@ def create(request):
     new_post = Post()
 
     new_post.title = request.POST.get('title')
-    new_post.writer = request.POST.get('writer')
+    new_post.writer = request.user
     new_post.email = request.POST.get('email')
     new_post.body = request.POST.get('body')
     new_post.pub_date = timezone.now()
@@ -63,4 +74,28 @@ def create(request):
 
     new_post.save()
 
+    words = new_post.body.split(' ')
+    tag_list = []
+
+    for w in words:
+        if len(w)>0:
+            if w[0] == '#':
+                tag_list.append(w[1:])
+
+    for t in tag_list:
+        tag, boolean = Tag.objects.get_or_create(name=t)
+        new_post.tags.add(tag.id)
+
     return redirect('main:detail', new_post.id) 
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'main/tag-list.html', {'tags' : tags })
+
+def tag_posts(request, tag_id):
+    tag = get_object_or_404(Tag, id=tag_id)
+    posts = tag.posts.all()
+    return render(request, 'main/tag-post.html', {
+        'tag' : tag,
+        'posts' : posts
+    })
