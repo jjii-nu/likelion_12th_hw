349cabe158b670efe5f266a33ba52f668466d59d
jjii-nu <tonywoo616@gamil.com>
2024-05-22 00:17:34 +0900

    hw5

--- project/main/views.py
+++ project/main/views.py
@@ -61,7 +61,7 @@ def delete(request, id):
     delete_post.delete()
     return redirect('main:secondpage')
 
-def delete2(request, id):
+def deleteComment(request, id):
     delete_comment = Comment.objects.get(pk=id)
     post = Post.objects.get(pk=delete_comment.post.id)
     delete_comment.delete()
